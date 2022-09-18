d88c987f0b3288e19f3fab50360704fd0707c88a
Felipe Gené <felipegenef@gmail.com>
2022-09-18 13:09:12 -0300

    add month data

--- src/useCases/GetData/GetDataService.js
+++ src/useCases/GetData/GetDataService.js
@@ -10,15 +10,23 @@ module.exports = class AddDataService {
     }
 
     async execute(userId, deviceId, dateFilter = "year") {
-        const data = await this.userRepository.getData(
-            userId,
-            deviceId,
-            dateFilter
-        );
+        const [data, yearData] = await Promise.all([
+            this.userRepository.getData(userId, deviceId, dateFilter),
+            this.userRepository.getData(userId, deviceId, "year"),
+        ]);
 
+        const today = moment();
         let formatedData = [];
+        let yearformatedData = [];
         for (const item of data) {
             formatedData = formatedData.concat(
+                item.metrics.map((i) => {
+                    return { ...i, insertData: item.insertData };
+                })
+            );
+        }
+        for (const item of yearData) {
+            yearformatedData = yearformatedData.concat(
                 item.metrics.map((i) => {
                     return { ...i, insertData: item.insertData };
                 })
@@ -28,12 +36,16 @@ module.exports = class AddDataService {
         let tensao = [];
         let potencia = [];
         let groupedData = {};
+        let tensaoAtual,
+            potenciaAtual,
+            correnteAtual = 0;
         // [{x:"Janeiro",y:20}]
         switch (dateFilter) {
             case "year":
                 var format = new Intl.DateTimeFormat("pt-br", {
                     month: "long",
                 });
+                let todayFormated = format.format(today);
                 groupedData = groupBy(
                     formatedData.map((item) => {
                         return {
@@ -75,6 +87,14 @@ module.exports = class AddDataService {
                             averageCorrente,
                         date,
                     });
+                    if (todayFormated == key) {
+                        potenciaAtual =
+                            averageCorrente *
+                            averageResistencia *
+                            averageCorrente;
+                        correnteAtual = averageCorrente;
+                        tensaoAtual = averageCorrente * averageResistencia;
+                    }
                 }
                 break;
 
@@ -174,9 +194,75 @@ module.exports = class AddDataService {
                 }
                 break;
         }
-        tensao = sortArray(tensao, (item) => item.date, "ASC");
-        potencia = sortArray(potencia, (item) => item.date, "ASC");
-        corrente = sortArray(corrente, (item) => item.date, "ASC");
-        return { tensao, corrente, potencia };
+        const tensaoOrdenada = sortArray(tensao, (item) => item.date, "ASC");
+        const potenciaOrdenada = sortArray(
+            potencia,
+            (item) => item.date,
+            "ASC"
+        );
+        const correnteOrdenada = sortArray(
+            corrente,
+            (item) => item.date,
+            "ASC"
+        );
+        potencia = [];
+        tensao = [];
+        corrente = [];
+        var format = new Intl.DateTimeFormat("pt-br", {
+            month: "long",
+        });
+        let todayFormated = format.format(today);
+        groupedData = groupBy(
+            yearformatedData.map((item) => {
+                return {
+                    ...item,
+                    x: format.format(item.insertData),
+                };
+            }),
+            "x"
+        );
+        for (const [key, values] of Object.entries(groupedData)) {
+            let totalResistencia = 0;
+            let totalCorrente = 0;
+            let averageCorrente = 0;
+            let averageResistencia = 0;
+            const [fistValue] = values;
+            const date = fistValue.insertData;
+            for (const metric of values) {
+                totalCorrente += metric.corrente;
+                totalResistencia += metric.resistencia;
+            }
+            averageCorrente = totalCorrente / parseFloat(values.length);
+            averageResistencia = totalResistencia / parseFloat(values.length);
+            corrente.push({
+                x: key,
+                y: averageCorrente,
+                date,
+            });
+            tensao.push({
+                x: key,
+                y: averageCorrente * averageResistencia,
+                date,
+            });
+            potencia.push({
+                x: key,
+                y: averageCorrente * averageResistencia * averageCorrente,
+                date,
+            });
+            if (todayFormated == key) {
+                potenciaAtual =
+                    averageCorrente * averageResistencia * averageCorrente;
+                correnteAtual = averageCorrente;
+                tensaoAtual = averageCorrente * averageResistencia;
+            }
+        }
+        return {
+            tensao: tensaoOrdenada,
+            corrente: potenciaOrdenada,
+            potencia: correnteOrdenada,
+            tensaoAtual,
+            correnteAtual,
+            potenciaAtual,
+        };
     }
 };
